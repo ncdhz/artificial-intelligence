27e1fa57fa12660e21b9562846d7654367570273
ncdhz <1137436221@qq.com>
2022-09-17 01:49:08 +0800

    add Sarsa

--- 强化学习/QLearning/maze_env.py
+++ 强化学习/QLearning/maze_env.py
@@ -1,22 +1,6 @@
-"""
-Reinforcement learning maze example.
-
-Red rectangle:          explorer.
-Black rectangles:       hells       [reward = -1].
-Yellow bin circle:      paradise    [reward = +1].
-All other states:       ground      [reward = 0].
-
-This script is the environment part of this example. The RL is in RL_brain.py.
-
-View more on my tutorial page: https://morvanzhou.github.io/tutorials/
-"""
 import numpy as np
 import time
-import sys
-if sys.version_info.major == 2:
-    import Tkinter as tk
-else:
-    import tkinter as tk
+import tkinter as tk
 
 
 UNIT = 40   # pixels
